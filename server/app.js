dabb288a488312370478f64063437d6105131e3c
LuisWislo <luisfwilson1998@gmail.com>
2020-06-10 23:33:48 -0500

    relocation

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://cholegod:F5ckM3D4dd7@cruzandoanimales-fk5mo.azure.mongodb.net/<dbname>?retryWrites=true&w=majority";
  
 app.get('/api/test', (req, res) => {
-    res.send('Asami the sexuality bender')
+    /*var toSend;
+    MongoClient.connect(uri, (err, client) => {
+        if(!err) {
+            const db = client.db('CruzandoAnimales');
+            const collection = db.collection('villagers');
+
+            collection.find().toArray((err, data) => {
+                toSend = data
+            })
+        }
+    });*/
+    res.send("carlos")
 })
  
 app.listen(3000, () => console.log('blog server running on port 3000!'))
